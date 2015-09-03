6d5cd4b167ec507768587684935d535c361cab5d
Mario Uher <uher.mario@gmail.com>
2015-09-03 17:45:42 +0200

    Adds react-router.

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ import "./style.css";
 
 
 import React from "react";
+import { Router, Route } from "react-router";
+import createHistory from "history/lib/createBrowserHistory";
 import App from "./components/App";
 
 
-React.render(<App />, document.body);
+React.render(
+  <Router history={createHistory()}>
+    <Route path="/" component={App} />
+  </Router>
+, document.body);
