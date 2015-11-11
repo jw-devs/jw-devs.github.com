428025b09353a316f17ca9b5a8893ed96dbea1ab
Mario Uher <uher.mario@gmail.com>
2015-11-11 12:35:55 +0100

    Activate source map support

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   output: {
     filename: "bundle.js"
   },
+  devtool: "inline-source-map",
   module: {
     loaders: [
       { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader?modules&importLoaders=1&localIdentName=[local]_[hash:base64:5]!postcss-loader") },
