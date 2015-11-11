d7c2d3fbf8a647bb1baefcf93590d1f6ac6b8a6e
Mario Uher <uher.mario@gmail.com>
2015-11-11 12:53:48 +0100

    Use CSS modules for body styles

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import "./style.css";
-
-
+import styles from "./style.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route } from "react-router";
 import createHistory from "history/lib/createBrowserHistory";
 import App from "./components/App";
 
+
+document.body.classList.add(styles.body);
 
 ReactDOM.render(
   <Router history={createHistory()}>
