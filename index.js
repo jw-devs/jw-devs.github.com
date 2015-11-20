36b1b814eebcd921ac9efce3bdbf4284672ede1b
Mario Uher <uher.mario@gmail.com>
2015-11-20 20:57:02 +0100

    Remove global style.css

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-import styles from "./style.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route } from "react-router";
 import createHistory from "history/lib/createBrowserHistory";
+
 import App from "./components/App";
 
-
-document.body.classList.add(styles.body);
 
 ReactDOM.render(
   <Router history={createHistory()}>
