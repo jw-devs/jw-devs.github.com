1f0a0070513ac602434b84268765ee4a7fd8c3d3
Mario Uher <uher.mario@gmail.com>
2015-11-20 20:52:54 +0100

    Adapt classNames

--- components/App/index.js
+++ components/App/index.js
@@ -1,11 +1,13 @@
 import styles from "./index.css";
+
 import React from "react";
+import classNames from "classnames";
 
 
 export default () => (
-  <section className={styles.Container}>
+  <section className={classNames(styles.Container)}>
     <div>
-      <img className={styles.Logo} src="logo.svg" alt="JW.devs Logo" />
+      <img className={classNames(styles.Logo)} src="logo.svg" alt="JW.devs Logo" />
       <p>
         Wir sind eine Programmierer-Community von Brüdern&nbsp;&amp;&nbsp;Schwestern.
       </p>
