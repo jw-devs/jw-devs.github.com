3c697e4cb68868d2c7263144214eeecf1c03844c
Mario Uher <uher.mario@gmail.com>
2015-11-20 20:57:53 +0100

    Remove react-router
    Might be a overkill.

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route } from "react-router";
-import createHistory from "history/lib/createBrowserHistory";
 
 import App from "./components/App";
 
 
-ReactDOM.render(
-  <Router history={createHistory()}>
-    <Route path="*" component={App} />
-  </Router>
-, document.querySelector("main"));
+ReactDOM.render((
+  <App />
+), document.querySelector("main"));
