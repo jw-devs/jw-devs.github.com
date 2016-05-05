f4b6471c1f7c70003e651c381c36eba9b980bbca
Mario Uher <uher.mario@gmail.com>
2016-05-05 11:34:18 +0200

    v2

---
+++ _index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render((
+  <pre>
+    JW.devs v2
+  </pre>
+), document.querySelector("main"));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./_index.js",
+  output: {
+    filename: "index.js",
+    path: __dirname
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader") },
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "_index.html", hash: true }),
+    new ExtractTextPlugin("bundle.css")
+  ],
+  postcss: [
+    require("autoprefixer")
+  ]
+};
